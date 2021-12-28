8e4dfe20e9955a1569895db88acb6e56a1159b6f
josephc2195 <chicaj1221@gmail.com>
2021-12-28 17:27:35 -0500

    update

--- api.py
+++ api.py
@@ -8,9 +8,32 @@ api = Api(app)
 
 class Users(Resource):
         def get(self):
-                data = pd.read_csv('/Datasets/users.csv')
+                data = pd.read_csv('https://raw.githubusercontent.com/josephc2195/Py-API/master/Datasets/users.csv')
                 data = data.to_dict()
                 return {'data': data}, 200
+        
+        def post(self):
+                parser = reqparse.RequestParser()
+                parser.add_argument('userId', required=True, type=int)
+                parser.add_argument('name', required=True)
+                parser.add_argument('city', required=True)
+                args = parser.parse_args()
+                
+                data = pd.read_csv('https://raw.githubusercontent.com/josephc2195/Py-API/master/Datasets/users.csv')
+                if args['userId'] in data['userId']:
+                        return {
+                                'message': f"{args['userId']} is already taken."
+                        }, 409
+                else:
+                        data=data.append({
+                                'userId':args['userId'],
+                                'name':args['name'],
+                                'city':args['city'],
+                                'locations':[]
+                        }, ignore_index=True)
+                        return {'data':data.to_dict()}, 200
+
+                
                 
 class Locations(Resource):
         pass
@@ -19,4 +42,4 @@ api.add_resource(Users, '/users')
 api.add_resource(Locations, '/locations')
 
 if __name__ == '__main__':
-        app.run()
+        app.run(debug=True)
